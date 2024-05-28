68f0070e17ea0a2e5d95c7131fae6eac75fd4015
Uf11 <itsusman11@gmail.com>
2024-05-28 18:24:06 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,23 @@ const App = () => {
   const [teams, setTeams] = useState([]);
 
   useEffect(() => {
-    // Fetch data from API
-    fetch('http://localhost:3001/teams')
-      .then(response => response.json())
-      .then(data => {
-        // Update state with fetched data
-        console.log(data)
-        setTeams(data.teams);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
+    // Function to fetch data from API
+    const fetchData = () => {
+      fetch('http://localhost:3001/teams')
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+          setTeams(data.teams);
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+    };
+    fetchData();
+
+    const intervalId = setInterval(fetchData, 5000);
+
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
